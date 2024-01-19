6f0c71ec820da62d613ec5e85a69026eb5a677bb
joeygitari <gitarijoanne@gmail.com>
2024-01-19 21:57:58 +0300

    IPs and siteC

--- siteBwindows.py
+++ siteBwindows.py
@@ -3,7 +3,7 @@ import psycopg2
 
 # Connecting to the MySQL database on the MAC machine
 mysql_database_connection = mysql.connector.connect(
-    host="",
+    host="192.168.100.9",
     port="3306",
     user="root",
     passwd="",
@@ -14,7 +14,7 @@ cursorObject = mysql_database_connection.cursor(buffered=True)
 
 # connecting to the PostgresSQL database on the MAC machine
 local_stream = psycopg2.connect(
-    host='',
+    host='192.168.100.9',
     port='5432',
     database='ddb-miniproject',
     user='',
