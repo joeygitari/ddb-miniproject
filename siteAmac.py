d76dd3b821e84aaa7bca115fd68e4eaeb49473e3
joeygitari <gitarijoanne@gmail.com>
2024-01-19 14:44:49 +0300

    siteAmac

---
+++ siteAmac.py
@@ -0,0 +1,27 @@
+import pyodbc
+import mysql.connector
+
+# Connecting to the MySQL database
+mysql_connection = mysql.connector.connect(
+    host="",
+    port="3306",
+    user="root",
+    passwd="",
+    database="ddb-miniproject"
+)
+
+# Preparing a cursor object for the MySQL database
+mysql_cursor = mysql_connection.cursor(buffered=True)
+
+# Connecting to the MSSQL database
+mssql_connection = pyodbc.connect(
+    driver='{ODBC Driver 17 for SQL Server}', 
+    server='Moh',
+    database='ddb-miniproject',
+    user='root',
+    password='',
+    trusted_connection='yes'
+)
+
+# Preparing a cursor object for the MSSQL database
+mssql_cursor = mssql_connection.cursor()
